104e56bfe0430ba0255dabfbd3545537984f6fba
DevLev7 <69923255+DevLev7@users.noreply.github.com>
2020-11-17 21:54:45 +0300

    2

---
+++ document/app.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("form_doc").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("form_doc").style.display = "none";
+  }
